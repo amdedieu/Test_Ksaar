99a66e42a7c3dd02e7ccf4b0aed36c0954dbd449
amdedieu <amdedieu@student.42.fr>
2022-04-28 14:32:46 +0200

    parsing ok, need fix in for()

--- soluce.js
+++ soluce.js
@@ -2,14 +2,16 @@
 function closerOfZero(tab){
 	if (!tab || tab.length == 0)
 		return 0;
-	let num = tab[0]; // will stock the closest number of zero
-	for (let i; i < tab.length; i++){
-		if(Maths.abs(num) > Maths.abs(tab[i])) // compare with absolute value to avoid negative number
-			tmp = tab[i];
-		if (Maths.abs(num) === Maths.abs(tab[i])){ // compare if the absolute value are equal
-			if (num < tab[i]) // then if the original num < tab[i] means there is a negative value in num && we want to return the positive one 
+	var num = tab[0]; // will stock the closest number of zero
+	for (let i; i <= tab.length; i++){
+		if(Maths.abs(num) > Maths.abs(tab[i])) // compare with absolute value to simplify the negative part
+			num = tab[i];
+		else if (Maths.abs(num) === Maths.abs(tab[i]) && num < tab[i]){ // compare if the absolute value are equal && if the original num is < tab[i]
 				num = tab[i] // so we assign the value of the original tab who is the positive closest number of 0
 		}
+		return num;
 	}
-	return num;
 }
+
+
+console.log(closerOfZero(0));
